93858708858f1370ad2bd86fe6c837bdd0018a73
JackF710 <jtfeery@gmail.com>
2025-03-06 13:01:59 -0500

    h

---
+++ app/src/components/SquirrelCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <h2>Color: {{ squirrel.primary_fur_color }}</h2>
+    <h2>Location: {{ squirrel.location }}</h2>
+    <h2>Was it Running?: {{ squirrel.running }}</h2>
+  </div>
+</template>
+
+<script setup>
+defineProps({ squirrel: Object })
+</script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,18 +1,26 @@
+<template>
+  <div class="container">
+    <h1>Squirrels in Central Park</h1>
+    <div class="card-container"></div>
+  </div>
+</template>
+
 <script setup>
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-const route = useRoute()
-const squirrel = ref('')
+
+const squirrel = ref([])
+
 async function getSquirrels() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
-  let data = await res.json()
-  squirrel.color = data.results
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+    squirrel.value = await response.json()
+  } catch (error) {
+    console.error('Error fetching squirrel data', error)
+  }
+  onMounted(() => {
+    getSquirrels
+  })
 }
-onMounted(() => {
-  getSquirrels
-})
 </script>
 
-<template>
-  <h1>hello boi</h1>
-</template>
+<style scoped></style>
